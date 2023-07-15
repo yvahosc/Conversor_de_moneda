e4241d4286891bd6208577e29a5887b3bd645442
Yeisson Augusto Vahos Cortes <yvahosc@unal.edu.co>
2023-07-14 22:36:45 -0500

    Creación de la estructura del método main para la verificación del funcionamiento del conversor utilizando la consola.

--- src/co/com/Oracle_Alura/Main.java
+++ src/co/com/Oracle_Alura/Main.java
@@ -1,10 +1,32 @@
 package co.com.Oracle_Alura;
 
+import co.com.Oracle_Alura.modelo.ConversorDeLongitud;
+import co.com.Oracle_Alura.modelo.ConversorDeMasa;
+import co.com.Oracle_Alura.modelo.ConversorDeMoneda;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		ConversorDeMoneda ejem1 = new ConversorDeMoneda("USD", "COP", 32.9);
+		System.out.println(ejem1.conversion());
+		
+		ConversorDeMoneda ejem2 = new ConversorDeMoneda("COP", "USD", 136908.74);
+		System.out.println(ejem2.conversion());
+		
+		ConversorDeLongitud ejem3 = new ConversorDeLongitud("m", "km", 1000);
+		System.out.println(ejem3.conversion());
+		
+		ConversorDeLongitud ejem4 = new ConversorDeLongitud("km", "m", 2.2);
+		System.out.println(ejem4.conversion());
+		
+		ConversorDeMasa ejem5 = new ConversorDeMasa("kg", "g", 1000);
+		System.out.println(ejem5.conversion());
+		
+		ConversorDeMasa ejem6 = new ConversorDeMasa("t", "kg", 2.2);
+		System.out.println(ejem6.conversion());
+		
+		
 	}
 
 }
